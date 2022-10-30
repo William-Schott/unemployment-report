bb195d0fbb8fd085dfb409cc039969017569c2b8
William-Schott <97321981+William-Schott@users.noreply.github.com>
2022-10-30 15:39:22 -0400

    Updated the fetching data part of the code and added import os

--- App/unemployment.py
+++ App/unemployment.py
@@ -3,9 +3,12 @@ from getpass import getpass
 API_KEY = getpass("Please input your AlphaVantage API Key: ") 
 #PCBWXAJKUC99DFN6
 
-import requests
+
+import os
 import json
 from pprint import pprint
+
+import requests
 
 request_url = f"https://www.alphavantage.co/query?function=UNEMPLOYMENT&apikey={API_KEY}"
 
