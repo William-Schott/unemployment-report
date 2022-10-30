b628e28524b04f76936d53b8aaa42999aa60611f
William-Schott <97321981+William-Schott@users.noreply.github.com>
2022-10-30 14:57:45 -0400

    Added part b

--- App/unemployment.py
+++ App/unemployment.py
@@ -21,3 +21,16 @@ print("-------------------------")
 print("LATEST UNEMPLOYMENT RATE:")
 #print(data[0])
 print(f"{data[0]['value']}%", "as of", data[0]["date"])
+
+
+#Part B
+from statistics import mean
+
+this_year = [d for d in data if "2022-" in d["date"]]
+
+rates_this_year = [float(d["value"]) for d in this_year]
+#print(rates_this_year)
+
+print("-------------------------")
+print("AVG UNEMPLOYMENT THIS YEAR:", f"{mean(rates_this_year)}%")
+print("NO MONTHS:", len(this_year))
