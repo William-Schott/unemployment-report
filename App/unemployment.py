1dc2c6c2cdf93d9e438db6423062ba73fb692237
William-Schott <97321981+William-Schott@users.noreply.github.com>
2022-10-31 12:42:30 -0400

    fixed some of the code for challenge a

--- App/unemployment.py
+++ App/unemployment.py
@@ -18,18 +18,18 @@ response = requests.get(request_url)
 
 parsed_response = json.loads(response.text)
 print(type(parsed_response))
-pprint(parsed_response)
+#pprint(parsed_response)
 
-
+data = parsed_response["data"]
 #Part A
 
-breakpoint()
+#breakpoint()
 
 latest = parsed_response["data"][0]
-pring(latest)
+print(latest)
 print("-------------------------")
 print("LATEST UNEMPLOYMENT RATE:")
-print(data[0])
+#print(data[0])
 print(f"{data[0]['value']}%", "as of", data[0]["date"])
 
 
