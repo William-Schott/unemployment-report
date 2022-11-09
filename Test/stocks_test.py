476a53bdac88b49f28bcbdc401f0295c5e7e4c10
William-Schott <97321981+William-Schott@users.noreply.github.com>
2022-11-09 17:19:23 -0500

    fixed the tests utility

--- Test/stocks_test.py
+++ Test/stocks_test.py
@@ -7,27 +7,34 @@
   
 #make sure that this is indented from the def
 from App.stocks import format_usd, fetch_stocks_data
+
 from pandas import DataFrame
+
+
 
 def test_usd_formatting():
 
+    #assert 2 + 2 == 4
 
     assert format_usd(4.5) == "$4.50"
 
-        ## the following two lines are edge cases
-    assert format_usd(4.555555) == "$4.56"
+    assert format_usd(4.5555555) == "$4.56"
 
-    assert format_usd(1234567890 == "$1,234,567,890.00")
+    assert format_usd(1234567890) == "$1,234,567,890.00"
 
-    def test_data_fetching():
-        result = fetch_stocks_data("NFLX")
-        assert isinstance(result,DataFrame)
-        assert "timestamp" in result.columns
-        assert"adjusted_close" in result.columns
-        assert "high" in result.columns
-        assert "low" in result.columns
+    #assert format_usd("OOPS") == "______"
 
-        assert len(result) >= 100
-    
-    ##test invalid inputs?
+
+
+def test_data_fetching():
+    result = fetch_stocks_data("NFLX")
+    assert isinstance(result, DataFrame)
+
+    assert "timestamp" in result.columns
+    assert "adjusted_close" in result.columns
+    assert "high" in result.columns
+    assert "low" in result.columns
+
+
+    assert len(result) >= 100    ##test invalid inputs?
    ### result = fetch_stocks_data("NFLX")
